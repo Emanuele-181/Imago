75dac97225d1d79de41869e1d215c9fed5be9ec1
Emanuele <bonifazi.1815292@studenti.uniroma1.it>
2020-05-14 13:12:22 +0200

    Modifiche tool.js e caricaImmagine.php.

--- view/Shared/caricaImmagine.php
+++ view/Shared/caricaImmagine.php
@@ -6,18 +6,20 @@
 
             /* Cartella di destinazione.
              * Cambiare per scegliere un'altra destinazione */
-            $destinazione = "../../public/Store";
-            
+            $destinazione = "../../public/Store/";
+
+            /* Nome del file caricato */
+            $file_name = basename($_FILES["inputImg"]["name"]);
+
             /* $_FILES contiene tutte le informazioni del file caricato
              * devo specificare a quale input mi riferisco e l'informazione che voglio */
             $nome_file_dst = $destinazione . basename($_FILES["inputImg"]["name"]);
-
             /* Variabile che controlla se l'upload è andato a buon fine */
             $uploadOk = 1;
 
             /* Controlo se il file esiste già */
             if(file_exists($nome_file_dst)){
-                echo "E' già presente un file con questo nome nel server";
+                echo "E' già presente un file con questo nome nel server.";
                 $uploadOk = 0;
             }
 
@@ -32,10 +34,14 @@
                  * l'immagine direttamente nella pagina html*/
                 if (move_uploaded_file($_FILES["inputImg"]["tmp_name"], $nome_file_dst)) {
                     echo "<script>
-                            $('#Img').load(function(){
-                                $image.attr('src'," . $nome_file_dst . ");
-                            });
-                        </script>";
+                                sessionStorage.Img ='". $file_name ."'
+                            </script>";
+                    echo "<script>
+                        window.history.back();".
+                            /* $('#Img').load(function(){
+                                $image.attr({src:" . $nome_file_dst . ", style:'width:50vw;border-radius: 10px;'});
+                            }); */
+                        "</script>";
                 }
                 else {
                     echo "Il file non può essere caricato, ci scusiamo per il disagio";
