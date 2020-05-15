c0cb28e5a2715d36373778cc7ca50560cf75b7cf
Emanuele <bonifazi.1815292@studenti.uniroma1.it>
2020-05-15 17:50:47 +0200

    Funzioni di modifica foto

--- view/Script/tool.js
+++ view/Script/tool.js
@@ -7,7 +7,8 @@ function visualizzaImg(){
         return;
     }
 
-    var img = $("<img />").attr({   src : '../../public/Store/'+sessionStorage.Img,
+    var img = $("<img />").attr({   id: 'Immagine',
+                                    src: '../../public/Store/'+sessionStorage.Img,
                                     style:"width:50vw;border-radius: 10px; box-shadow: 4px 4px 8px"}).on('load', function() {
         if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
             alert('Attenzione! Immagine non caricata!');
@@ -27,4 +28,63 @@ function controlloPresenzaImg(){
     if(sessionStorage.Img){
         document.getElementById('formCarica').style.display = 'none';
     }
+}
+
+
+
+
+
+
+
+/* *************************************** */
+/* Funzioni per la modifica delle immagini */
+/* *************************************** */
+
+/* Variabili */
+var default_value = {luminosita_var : 1, contrasto_var : 100};
+var obj_mod = {luminosita_var : 1, contrasto_var : 100};
+
+
+/*var contrasto_var = 100;
+var luminosita_var = 1;*/
+
+
+/* Funzioni */
+function reset(){
+    obj_mod = JSON.parse(JSON.stringify(default_value));
+    var value = 'brightness('+ (Math.floor(obj_mod.luminosita_var * 10) / 10).toString() +') contrast('+ obj_mod.contrasto_var.toString() + '%' +')';
+    $('#Immagine').css('filter', value);
+}
+
+
+function contrasto(cmd){
+    if(cmd == '+'){
+        obj_mod.contrasto_var = obj_mod.contrasto_var + 10;
+        var str = obj_mod.contrasto_var.toString() + '%';
+        var value = 'brightness('+ (Math.floor(obj_mod.luminosita_var * 10) / 10).toString() +') contrast('+ str +')';
+        $('#Immagine').css('filter', value);
+    }
+    else{
+        obj_mod.contrasto_var = obj_mod.contrasto_var - 10;
+        var str = obj_mod.contrasto_var.toString() + '%';
+        var value = 'brightness('+ (Math.floor(obj_mod.luminosita_var * 10) / 10).toString() +') contrast('+ str +')';
+        $('#Immagine').css('filter', value);
+    }
+    
+}
+
+function luminosita(cmd){
+    if(cmd == '+'){
+        obj_mod.luminosita_var = obj_mod.luminosita_var + 0.1;
+        var str = (Math.floor(obj_mod.luminosita_var * 10) / 10).toString();
+        var value = 'brightness('+ str +') contrast('+ obj_mod.contrasto_var.toString() + '%' +')';
+        $('#Immagine').css('filter', value);
+    }
+    else{
+        obj_mod.luminosita_var = obj_mod.luminosita_var - 0.1;
+        var str = (Math.floor(obj_mod.luminosita_var * 10) / 10).toString();
+        var value = 'brightness('+ str +') contrast('+ obj_mod.contrasto_var.toString() + '%' +')';
+        $('#Immagine').css('filter', value);
+    }
+    
 }
