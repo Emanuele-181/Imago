7ca1be65dbc1cda64f0921188011acb78b8769fe
Emanuele <bonifazi.1815292@studenti.uniroma1.it>
2020-05-19 17:54:02 +0200

    Page bloccate

--- view/Script/cookies.js
+++ view/Script/cookies.js
@@ -10,8 +10,9 @@
 /* Controlla se un utente è loggato */
 function checkLogin(){
     if(sessionStorage.logged != 'true' && localStorage.logged != 'true'){
+        $('body').css('filter', 'blur(10px)');
         alert('Per accedere a questa pagina devi registrarti o fare il login.');
-        if(window.history.length == 1){
+        if(window.history.length <= 2){
             window.location.replace('../Sito/HomeSito.html');
         }
         else{
