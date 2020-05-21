c015fa3462e06e135dbdc53b7625bd6e19811cf4
salvatoreiorianni <64103230+salvatoreiorianni@users.noreply.github.com>
2020-05-21 16:38:16 +0200

    Aggiunte funzioni js controllo form registrazione

--- view/Script/ScriptNavbarPopup.js
+++ view/Script/ScriptNavbarPopup.js
@@ -13,6 +13,50 @@ function riuscito(){
     alert("Pagamento effettuato con successo!");
 }
 
+function checkNome(){
+    if(!isNaN(document.formRegistrazione.nome.value)) {
+        alert("Attenzione Il Nome NON può essere un numero!");
+        return false;
+    }
+    return true;
+}
 
+function checkCognome(){
+    if(!isNaN(document.formRegistrazione.cognome.value)) {
+        alert("Attenzione Il Cognome NON può essere un numero!");
+        return false;
+    }
+    return true;
+}
+
+function validazioneForm(){
+    if(document.formRegistrazione.nome.value=="" || document.formRegistrazione.cognome.value=="" || document.formRegistrazione.email.value=="" || document.formRegistrazione.password.value=="") {
+        alert("Verifica che tutti i campi siano compilati!");
+        return false;
+
+    }
+
+    alert("Dati Inseriti Correttamente!");
+    return true;
+}
+
+function checkPassowrd(){
+    var password= document.formRegistrazione.password.value;
+    var presenzaNumero=false;
+    for(i=0;i<=9;i++) {
+        if(password.indexOf(i) >=0) {
+            presenzaNumero=true;
+        }
+    }
+
+    if(presenzaNumero==false){
+        alert("inserisci almeno un numero nella password!!");
+        return false;
+    }
+    
+    return true;
+        
+    
+}
 
 
